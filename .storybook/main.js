93eba67c460fdbe5e8203ec8a6f036ef0b27aa36
Darren Sorrels <darrenbydesign@dgmail.com>
2022-09-14 21:37:13 -0500

    commits uswds and test components to the test project

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
+    '@storybook/addon-a11y'
   ],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
--- apps/money-maker/src/app/app.module.ts
+++ apps/money-maker/src/app/app.module.ts
@@ -3,9 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonComponent } from './button/button.component';
+import { ButtonGroupComponent } from './button-group/button-group.component';
+import { AlertComponent } from './alert/alert.component';
+import { ModalComponent } from './modal/modal.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ButtonComponent,
+    ButtonGroupComponent,
+    AlertComponent,
+    ModalComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
